7006907c48ce536bfcd4013a88abbe72eb512fdb
Velizar Bishurov <vbishurov@gmail.com>
2014-12-05 08:11:46 +0200

    homework change

--- PHP-Forms-Homework/02.MostFrequentTag.php
+++ PHP-Forms-Homework/02.MostFrequentTag.php
@@ -2,20 +2,14 @@
 $pageTitle = 'Most Frequent Tags';
 include 'includes/header.php';
 ?>
-<form action="02.MostFrequentTag.php" method="post">
-<input type="text" name="input">
-<input type="submit">
+    <form action="02.MostFrequentTag.php" method="post">
+        <input type="text" name="input">
+        <input type="submit">
 <?php
 $input = preg_split('/,\s+/', htmlentities($_POST['input']), NULL, PREG_SPLIT_NO_EMPTY);
 $result = [];
-$appearances = 0;
-for ($i = 0; $i < count($input); $i++) {
-    if (!$result[$input[$i]]) {
-        $result[$input[$i]] = 1;
-    } else {
-        $result[$input[$i]]++;
-    }
-}
+$input = preg_split('/,\s+/', $_POST['input'], NULL, PREG_SPLIT_NO_EMPTY);
+$result = array_count_values($input);
 arsort($result);
 $mostFrequent = key($result);
 echo "<br>";
